3cabe461cfb877759ef5c6fe7193ca21ace2b807
Felix <phong.danghong.dev@gmail.com>
2023-06-10 19:38:44 +0700

    add docs and redoc

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd app
+    "rest_framework",
+    "drf_spectacular",
+
     # custom app
     'core',
 ]
@@ -126,3 +130,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
